8845644d5f53c93640a21bac3c3703fa3b78455f
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-11-29 16:35:47 -0300

    usando useEffect para pegar a api dos repositorios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,8 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
+
+//https://api.github.com/users/celiovjunior/repos
 
 const repository = {
     name: 'unform',
@@ -8,6 +11,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.github.com/users/celiovjunior/repos")
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [])
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
